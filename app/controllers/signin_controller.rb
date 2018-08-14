bfe263468578098f3bd60dd21aa2f9ae02e3cc8c
chadwalt <chadwalt04@gmail.com>
2018-08-14 12:55:18 +0300

    Ignore .idea files

---
+++ app/controllers/signin_controller.rb
@@ -0,0 +1,5 @@
+class SigninController < ApplicationController
+  def index
+    @title = 'welcome'
+  end
+end
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    uid '12343232'
+    provider 'google'
+    name 'kyadondo timothy'
+    email 'timothy.kyadondo@andela.com'
+    image_url 'photo.jpg'
+  end
+end
