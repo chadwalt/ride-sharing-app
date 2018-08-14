aa8bd3865fcd43c3f4030f1960be5dbd3ea7b434
chadwalt <chadwalt04@gmail.com>
2018-08-14 20:24:02 +0300

    Refactor controller tests

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -1,31 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe OmniauthCallbacksController, type: :controller do
-  before do
-    request.env["devise.mapping"] = Devise.mappings[:user]
-    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+  describe 'Successful Authentication' do
+    login_user
+
+    it 'should redirect to root on successful signin' do
+      get :google_oauth2
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to root_path
+    end
   end
 
-  it 'should redirect to root on successful signin' do
-    get :google_oauth2
-    expect(response).to have_http_status(:redirect)
-    expect(response).to redirect_to root_path
-  end
+  describe 'Unsuccessful Authentication' do
+    invalid_user
 
-  it 'should redirect to sigin on failure' do
-    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
-      :provider => 'google_oauth2',
-      :uid => '',
-      :info => {
-        name: 'test',
-        email: '',
-        image_url: 'http:www//google.com/picture'
-      }
-    })
-    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
-
-    get :google_oauth2
-    expect(response).to have_http_status(:redirect)
-    expect(response).to redirect_to root_path
+    it 'should redirect to sigin on failure' do
+      get :google_oauth2
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to root_path
+    end
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     uid '12343232'
     provider 'google'
-    name 'kyadondo timothy'
-    email 'timothy.kyadondo@andela.com'
+    name 'test'
+    email 'test@gmail.com'
     image_url 'photo.jpg'
   end
 end
