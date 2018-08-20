e77f493832bbb439b27d9e894cf30e9f966b7f99
chadwalt <chadwalt04@gmail.com>
2018-08-20 23:32:18 +0300

    Create tests for ride offer interests

---
+++ spec/models/ride_offer_interest_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe RideOfferInterest, type: :model do
+  describe 'Ride Offer Interest Model' do
+    it 'is not valid without a ride_offer_id' do
+      ride_offer_interest = RideOfferInterest.new(ride_offer_id: nil)
+      expect(ride_offer_interest).to_not be_valid
+    end
+
+    it 'is not valid without a user_id' do
+      ride_offer_interest = RideOfferInterest.new(user_id: nil)
+      expect(ride_offer_interest).to_not be_valid
+    end
+
+    it 'should save data on valid attributes' do
+      create(:user)
+      create(:ride_offer)
+      ride_offer_interest = RideOfferInterest.create(ride_offer_id: 1, user_id: 2)
+      expect(ride_offer_interest).to be_valid
+    end
+  end
+end
