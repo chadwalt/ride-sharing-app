13d3530af74c01cb64e286a98000a14bf35ef8ce
chadwalt <chadwalt04@gmail.com>
2018-08-22 15:12:50 +0300

    Test count for number of people who have shown interest in ride offer

--- spec/models/ride_offer_interest_spec.rb
+++ spec/models/ride_offer_interest_spec.rb
@@ -18,5 +18,12 @@ RSpec.describe RideOfferInterest, type: :model do
       ride_offer_interest = RideOfferInterest.create(ride_offer_id: 1, user_id: 2)
       expect(ride_offer_interest).to be_valid
     end
+
+    it 'should return number of counts of people interested in a ride offer' do
+      create(:user)
+      create(:ride_offer)
+      create(:ride_offer_interest)
+      expect(RideOfferInterest.signed_up_counts(1)).to eq 1
+    end
   end
 end
