58e05f0f47c0053a162bc6006213e126a46ebca7
chadwalt <chadwalt04@gmail.com>
2018-08-30 15:24:01 +0300

    Withdraw interest in ride offer

--- app/assets/javascripts/shared_ride_offers.coffee
+++ app/assets/javascripts/shared_ride_offers.coffee
@@ -11,6 +11,7 @@ $ ->
       location.reload()
     , 'JSON')
     .fail (response) ->
+      display_withdraw_interest()
       $('.modal-body').html(response.responseJSON.message)
 
   $('.show_interest_ride_offer').click (event) ->
@@ -19,6 +20,7 @@ $ ->
     message += "<b>#{ride_offer.origin}</b> to <b>#{ride_offer.destination}</b> "
     message += "at <b>#{ride_offer.take_off.slice(11, 19)}</b>?"
     interested_counts = $(this).data('interestedcounts')
+    display_show_interest_button()
 
     if ride_offer.no_of_people == interested_counts
       message = 'Maximum number of people reached.'
@@ -28,3 +30,24 @@ $ ->
       $('#show-interest').data('ride_offer_id', ride_offer.id)
 
     $('.modal-body').html(message)
+
+  $('#withdraw-interest').click ->
+    ride_offer_id = $('#show-interest').data('ride_offer_id')
+
+    $.ajax({
+      url: "/shared_ride_offers/#{ride_offer_id}",
+      method: 'DELETE',
+      dataType: 'JSON'
+    }).done((response) ->
+      location.reload()
+    ).fail((response) ->
+      display_show_interest_button()
+    )
+
+display_show_interest_button = () ->
+  $('#show-interest').show();
+  $('#withdraw-interest').hide();
+
+display_withdraw_interest = () ->
+  $('#withdraw-interest').show();
+  $('#show-interest').hide();
--- app/controllers/shared_ride_offers_controller.rb
+++ app/controllers/shared_ride_offers_controller.rb
@@ -1,6 +1,6 @@
 class SharedRideOffersController < ApplicationController
   before_action :authenticate_user!
-  skip_before_action :verify_authenticity_token, :only => [:show_interest]
+  skip_before_action :verify_authenticity_token, :only => [:show_interest, :destroy]
 
   # GET index
   def index
@@ -17,6 +17,12 @@ class SharedRideOffersController < ApplicationController
     @ride_offer = RideOffer.find_by(id: params[:id])
 
     if @ride_offer
+      if RideOfferInterest.shown_interest?(@ride_offer.id, current_user.id)
+        message = 'You have already shown interest in this ride offer. Would you like to withdraw your interest?'
+        respond_with_json({ message: message }, :not_acceptable)
+        return
+      end
+
       if maximum_no_of_people_reached? @ride_offer
         respond_with_json({ message: 'Maximum number of people reached' }, :not_acceptable)
         return
@@ -26,6 +32,17 @@ class SharedRideOffersController < ApplicationController
       respond_with_json({ message: 'Ride offer interest saved' }, :created)
     else
       respond_with_json({ message: 'Ride offer not found' }, :not_found)
+    end
+  end
+
+  def destroy
+    @ride_offer_interest = RideOfferInterest.find_by(ride_offer_id: params[:id], user_id: current_user.id)
+
+    if @ride_offer_interest
+      @ride_offer_interest.destroy
+      respond_with_json({ message: 'Interest withdrawn' }, :accepted)
+    else
+      respond_with_json({ message: 'Something happend please try again'}, :service_unavailable)
     end
   end
 
